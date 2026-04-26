1e9ff5c884415e470a617f3e5d5333a3bf825cb7
jesuisstan <stan.krivtsov@gmail.com>
2026-04-26 15:25:47 +0200

    app.config.js

---
+++ app.config.js
@@ -0,0 +1,58 @@
+module.exports = () => ({
+  expo: {
+    name: "examen-civique-naturalisation-francaise",
+    slug: "examen-civique-naturalisation-francaise",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "examenciviquenaturalisationfrancaise",
+    userInterfaceStyle: "automatic",
+    ios: {
+      icon: "./assets/expo.icon",
+    },
+    android: {
+      adaptiveIcon: {
+        backgroundColor: "#E6F4FE",
+        foregroundImage:
+          "./assets/images/android-icon-foreground.png",
+        backgroundImage:
+          "./assets/images/android-icon-background.png",
+        monochromeImage:
+          "./assets/images/android-icon-monochrome.png",
+      },
+      predictiveBackGestureEnabled: false,
+      package:
+        "com.jesuisstan"
+        + ".examenciviquenaturalisationfrancaise",
+    },
+    web: {
+      output: "static",
+      favicon: "./assets/images/favicon.png",
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          backgroundColor: "#208AEF",
+          android: {
+            image: "./assets/images/splash-icon.png",
+            imageWidth: 76,
+          },
+        },
+      ],
+    ],
+    experiments: {
+      typedRoutes: true,
+      reactCompiler: true,
+    },
+    extra: {
+      router: {},
+      eas: {
+        projectId:
+          "487fd6d2-daf5-4f58-8c41-1b817f19d5c0",
+      },
+    },
+    owner: "jesuisstan",
+  },
+});
